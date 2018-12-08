2b15d8e9d45f9b6ededf9ba98fd2755ee20eb6fe
Chandira Withanage <withana587@gmail.com>
2018-12-08 15:47:43 -0600

    add method

--- src/MySet.java
+++ src/MySet.java
@@ -1,8 +1,15 @@
 
 public class MySet {
-
+	Object[] objArray = new Object[10];
+	int count = 0;
+	
 	public boolean isEmpty() {
-		return false;
+		return count == 0;
+	}
+
+	public void add(Object obj) {
+		objArray[count] = obj;
+		count ++;
 	}
 
 }
--- test/MySetTest.java
+++ test/MySetTest.java
@@ -1,13 +1,24 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class MySetTest {
+	MySet mySet = new MySet();
+	
+	@Before
+	public void runBefore(){
+		
+	}
 
 	@Test
-	public void test() {
-		MySet mySet = new MySet();
+	public void isEmpty() {
 		assertTrue(mySet.isEmpty());
 	}
 
+	@Test
+	public void add() {
+		mySet.add("a");
+		assertFalse(mySet.isEmpty());
+	}
 }
